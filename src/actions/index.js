c8e2d17cbba37ad79e242243fd4e0bf1537ca6dc
Jamie McKenzie <jmckenzie@pandora.com>
2018-04-04 15:26:37 -0700

    oops

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
-const API_KEY = '841f9c287e5015e0bea70dace0df4775';
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
